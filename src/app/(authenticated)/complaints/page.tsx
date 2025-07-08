837ed61dadc3ab99a6cd17b094dbf0a7221a7118
toufic abi younes <tabiyounes@sollyazar.com>
2025-07-08 14:44:04 +0200

    bug fix

--- src/app/(authenticated)/complaints/page.tsx
+++ src/app/(authenticated)/complaints/page.tsx
@@ -1,7 +1,4 @@
 import ComplaintPage from "@/features/complaints-page/complaint-page";
-import { GetAllComplaints } from "@/features/complaints-page/complaints-services/complaint-service";
-import { DisplayError } from "@/features/ui/error/display-error";
-
 export default function Page() {
   return <ComplaintPage />;
 }
